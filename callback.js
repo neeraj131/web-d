91914f8410fd947a8cbb081d661fb0214f75ab70
neeraj131 <neerajmehra131@gmail.com>
2022-01-27 22:49:33 +0530

    first commit

---
+++ callback.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const cheerio= require('cheerio')
+
+request('https://www.worldometers.info/coronavirus/', cb);
+
+function cb(error, response, html){
+    if(error){
+        console.log(error);
+    }
+    else
+    handlehtml(html)
+}
+
+function handlehtml(html){
+    let seltool= cheerio.load(html)
+    let contentarr= seltool('.maincounter-number span')
+    let data= seltool(contentarr[0]).text();
+    let deaths = seltool(contentarr[1]).text();
+    let recovery = seltool(contentarr[2]).text();
+    console.log("Total no. of Covid cases : " + data);
+    console.log("Total no. of Deaths cases : "  + deaths);
+    console.log("Total no. of Recovery cases : " + recovery);
+
+}
